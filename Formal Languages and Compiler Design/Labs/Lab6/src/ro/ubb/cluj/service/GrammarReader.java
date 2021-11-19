01082f2ed5b4665aa0ceee63d4f8a6384525a6c7
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-11-19 13:51:47 +0200

    Initial commit parser

---
+++ Formal Languages and Compiler Design/Labs/Lab6/src/ro/ubb/cluj/service/GrammarReader.java
@@ -0,0 +1,9 @@
+package ro.ubb.cluj.service;
+
+public class GrammarReader {
+    private final String filePath;
+
+    public GrammarReader(String filePath) {
+        this.filePath = filePath;
+    }
+}
